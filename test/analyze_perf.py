cc8123db9be384738fa8928410890ac8d88fb238
Magnus Holm <judofyr@gmail.com>
2020-04-09 19:48:39 +0200

    Use curve_fit for fitting curve

--- test/analyze_perf.py
+++ test/analyze_perf.py
@@ -1,17 +1,19 @@
 import numpy as np
+import scipy.optimize as opt
 import sys
 
 # We assume time = C * n^k
 # log(time) = log(C) + k*log(n)
 
+def model(n, C, k):
+    return C * n**k
+
 data = np.loadtxt(sys.argv[1])
 n = data[:, 0]
 time = data[:, 1]
 
-coeffs = np.polyfit(np.log(n), np.log(time), 1)
-
-k = coeffs[0]
-C = np.exp(coeffs[1])
+popt, pcov = opt.curve_fit(model, n, time)
+C, k = popt
 
 print("C = %g" % C)
 print("n^{%.3f}" % k)
